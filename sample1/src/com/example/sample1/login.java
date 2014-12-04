9495bfa7df10a35812be5d7fde2853b78f9770ef
prathyushaV <vankapratyusha@gmail.com>
2014-12-04 13:58:35 +0530

    sample1 for android

---
+++ sample1/src/com/example/sample1/login.java
@@ -0,0 +1,75 @@
+package com.example.sample1;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.os.Build;
+
+public class login extends ActionBarActivity {
+	String login(String uname, String pwd){
+		return uname;
+	}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_success);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.login_container, new PlaceholderFragment())
+                    .commit();
+        }
+        Intent intent = getIntent();
+        String uname = intent.getStringExtra("uname");
+        TextView welcome_text = (TextView) findViewById(R.id.welcome_text);
+        welcome_text.setText("Welcome "+uname);
+    }
+
+   /* @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }*/
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.login_success, container, false);
+            return rootView;
+        }
+    }
+
+}
